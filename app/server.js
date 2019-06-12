cdf5405e2275b0619f70fae34e0930e57da978b6
Trash Panda <d.george.schlosser@gmail.com>
2019-06-12 02:45:06 -0400

    server complete

--- app/server.js
+++ app/server.js
@@ -11,6 +11,14 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
+//Serve static files w/ Express (https://expressjs.com/en/starter/static-files.html)
+app.use(express.static(path.join(__dirname, "./app/public")));
+
+// Routes
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
 // Starts server to begin listening
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
