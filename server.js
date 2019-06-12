cc5e55d567639f498467d763b30d5f20ba17bf14
Trash Panda <d.george.schlosser@gmail.com>
2019-06-12 02:05:33 -0400

    server functional

--- server.js
+++ server.js
@@ -7,11 +7,7 @@ var bodyParser = require("body-parser");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-// Middleware(body-parser)
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-// Starts server to begin listening
+// Starts the server to begin listening
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
-});
+  });
