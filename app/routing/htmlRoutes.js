1b51712f39e51d6439abb460a0f749ad91138489
Trash Panda <d.george.schlosser@gmail.com>
2019-06-12 22:25:38 -0400

    htmlRoutes edits

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,15 +1,16 @@
 // Dependencies
 var path = require("path");
 
+// Routes
 module.exports = function(app){
     //GET Route to /survey that displays the survey.html
     app.get("/survey", function(req, res) {
-        res.sendFile(path.join(__dirname + "/../public/survey.html"))
+        res.sendFile(path.join(__dirname, "../public/survey.html"))
     });
 
     //Default, catch-all route that leads to home.html
-    app.use(function(req, res) {
-        res.sendFile(path.join(__dirname + "/../public/home.html"));
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 };
 
